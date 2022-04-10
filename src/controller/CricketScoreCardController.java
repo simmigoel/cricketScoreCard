7c3f3867cc99d913a9e5608188e697a974eff036
simmigoel <103354894+simmigoel@users.noreply.github.com>
2022-04-10 14:58:40 +0530

    Update CricketScoreCardController.java

--- src/controller/CricketScoreCardController.java
+++ src/controller/CricketScoreCardController.java
@@ -1,1 +1,23 @@
 
+@RestController
+@RequestMapping(value=BASE_URL)
+public class CricketScoreCardController{
+  
+ private static final Logger logger = LoggerFactory.getLogger(CricketScoreCardController.class); 
+  
+  @Autowired
+  private CricketScoreService cricketScoreService;
+  
+  @GetMapping(value=GET_CRICKET_SCORE_CARD)
+  public @ResponseBody getCricketScoreCard(){
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+}  
+  
