f75c0986f6d0113a4e74791dca719c6824792b47
simmigoel <103354894+simmigoel@users.noreply.github.com>
2022-04-10 14:39:36 +0530

    Update Player.java

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,1 +1,11 @@
-
+@Data
+@Entity
+class Player{
+private Integer playerId;
+private String playerName;
+private Integer totalRuns
+private Integer numberOfFours
+private Integer numberOfSixes
+private Integer numberOfBalls
+private Integer battingOrder  
+}
